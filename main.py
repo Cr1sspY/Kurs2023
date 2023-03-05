9652c518190b034dfa0612f31970ffdd54559d69
Cr1sspY <sentluiz14@gmail.com>
2023-03-06 00:15:12 +0300

    тык

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-# Здесь будет основной код программы
+import sqlite3
+import sys
+from datetime import datetime
+
+from PyQt5 import uic
+from PyQt5.QtSql import QSqlDatabase, QSqlQueryModel
+from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QLineEdit
+
+
+class AuthWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = uic.loadUi("forms/auth.ui", self)
+        self.ui.show()
+        self.setWindowTitle('Авторизация')
